8fb23f967aa94b1f8c714c80e62fb6ab0ac1ca47
anjaboettcher <anjaverenaboettcher@gmail.com>
2019-07-24 16:32:26 +0100

    first commit

---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h2>Home</h2>
+      <p>This is a sample project with the MERN stack</p>
+
+      <p>
+        <strong>TODO: try another way to manage inputs</strong>
+      </p>
+    </div>
+  )
+}
