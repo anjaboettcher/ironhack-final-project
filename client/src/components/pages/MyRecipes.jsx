f348f98a2c3223bae32c4a395e237b263e98a78b
Sebastien Morelle <smorel14@gmail.com>
2019-07-27 16:41:48 +0100

    seb

--- client/src/components/pages/MyRecipes.jsx
+++ client/src/components/pages/MyRecipes.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react'
 //import axios from 'axios'
 import api from '../../api.js'
 import { Container, Row, Col } from 'reactstrap'
+import { MDBCol, MDBIcon } from 'mdbreact'
 
 export default function MyRecipes() {
   const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     api.getRecipes().then(info => {
@@ -15,12 +17,72 @@ export default function MyRecipes() {
     })
   }, [])
 
+  function handleChange(e) {
+    setSearch(e.target.value)
+    console.log('search', search)
+  }
+
+  // This is the search bar
+  function filterBySearch(allRecipes) {
+    return allRecipes.filter(
+      recipe =>
+        recipe.name.toUpperCase().includes(search.toUpperCase()) ||
+        recipe.categories
+          .join()
+          .toUpperCase()
+          .includes(search.toUpperCase())
+    )
+
+    //Way number 2 of doing it!
+    // let recipeList = []
+    // for (let j = 0; j < allRecipes.length; j++) {
+    //   if( allRecipes[j].name.toUpperCase().includes(search.toUpperCase())) {
+    //     recipeList.push(allRecipes[j])
+    //   }
+    //   else {
+    //   for (let k = 0; k < allRecipes[j].categories.length; k++) {
+    //     if (
+    //       allRecipes[j].categories[k]
+    //         .toUpperCase()
+    //         .includes(search.toUpperCase())
+    //     ) {
+    //       recipeList.push(allRecipes[j])
+    //     }
+    //   }
+    //   }
+    // }
+    // console.log('recipeList', recipeList)
+    // return recipeList
+  }
+
   return (
     <div>
-      <div>Search Bar</div>
+      <div class="border">
+        <MDBCol md="6">
+          <div className="input-group md-form form-sm form-1 pl-0">
+            <div className="input-group-prepend">
+              <span
+                className="input-group-text green lighten-3"
+                id="basic-text1"
+              >
+                <MDBIcon className="text-white" icon="search" />
+              </span>
+            </div>
+            <input
+              className="form-control my-0 py-1"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+              value={search}
+              onChange={handleChange}
+            />
+          </div>
+        </MDBCol>
+      </div>
       <Container>
-        {recipes.map((recipe, i) => (
+        {filterBySearch(recipes).map((recipe, i) => (
           <Row key={i} className="m-2">
+            {/* {recipe.categories} */}
             <Col className=" box m-2">
               <img
                 className="image"
